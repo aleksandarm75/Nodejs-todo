652f1f331e0bd9cd19c3cfaadb5050dd1fefa23f
AVANADE-CORP\aleksandar.milosevic <aleksandar.milosevic>
2022-10-31 15:35:46 +0100

    winston express & winston implementation

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
-//dependencies required for the app
+// Dependencies required for the app
 var express = require("express");
 var bodyParser = require("body-parser");
+var expressWinston = require('express-winston');
+var winston = require('winston'); // for transports.Console
+
+const session = require('express-session');
 const logger = require("./utils/logger");
+const router = express.Router();
+
+
 var app = express();
 
+// Global session variable
+global.globalSessionID = "";
+
+app.use(session({
+    secret: 'y7t7Ld3rQc',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 30
+    }
+}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-// global variables
+// express-winston logger makes sense BEFORE the router
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+            filename: 'logs/todolog.log'
+        })
+    ],
+    format: winston.format.combine(
+        winston.format.label({
+            label: 'API'
+        }),
+        winston.format.simple(),
+        winston.format.json(),
+        winston.format.timestamp({
+            format: 'DD-MM-YYYY HH:mm:ss:SSS'
+        }),
+        winston.format.printf(info => {
+            var reqObj = JSON.stringify(info.meta);
+            var reqEndpoing = info.meta.req.url;
+            var reqMethod = info.meta.req.method;
+
+            let out = `[${info.label}][${info.level}][ReqId:${globalSessionID}][${[info.timestamp]}][${reqMethod}:${reqEndpoing}] ${reqObj}`;
+            return out;
+        })
+    ),
+    meta: true
+}));
+
+// Now we can tell the app to use our routing code:
+app.use(router);
+
 var task = [];
 var complete = [];
 
+app.get('/', (req, res, next) => {
+    globalSessionID = req.sessionID;
+    next();
+});
+
 // post route for adding new task 
 app.post("/addtask", function (req, res) {
-    logger.info("Added task triggered!");
-    logger.warn("Some warrning message");
-    logger.debug("Some debug info");
-    logger.error("Some error message");
-
     var newTask = req.body.newtask;
     logger.info("Added new task name : " + JSON.stringify(req.body, null, 2));
     if (newTask == '') return;
@@ -27,6 +76,8 @@ app.post("/addtask", function (req, res) {
 
 // post route for removing tasks 
 app.post("/removetask", function (req, res) {
+    globalSessionID = req.sessionID;
+
     logger.info("Removed task triggered!");
     var completeTask = req.body.removed;
     logger.info("Removed task name : " + JSON.stringify(req.body, null, 2));
--- utils/logger.js
+++ utils/logger.js
@@ -1,21 +1,31 @@
 const winston = require('winston');
-const { createLogger, format, transports } = require('winston');
+const { createLogger, format } = require('winston');
 
 const logConfiguration = {
-    'transports': [
+    transports: [
+        new winston.transports.Console(),
         new winston.transports.File({
             filename: 'logs/todolog.log'
         })
     ],
     format: winston.format.combine(
         winston.format.label({
-            label: '👉'
+            label: 'APP'
         }),
         winston.format.simple(),
         winston.format.timestamp({
-            format: 'MMM-DD-YYYY HH:mm:ss'
+            format: 'DD-MM-YYYY HH:mm:ss:SSS'
         }),
-        winston.format.printf(info => `${info.level}: ${info.label} ${[info.timestamp]}: ${info.message}`),
+        winston.format.printf(info => {
+            if (globalSessionID === "") {
+                let out = `[${info.label}][${info.level}][${[info.timestamp]}] ${info.message}`;
+                return out;
+            }
+            else {
+                let out = `[${info.label}][${info.level}][ReqId:${globalSessionID}][${[info.timestamp]}] ${info.message}`;
+                return out;
+            }
+        })
     )
 };
 
